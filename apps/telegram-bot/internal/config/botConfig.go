abeb6b4e19adb881bb7612034ed89207e7822b0d
Aliaksei Makarevich <electron2686@gmail.com>
2026-06-29 20:34:53 +0300

    config in tele-bot

---
+++ apps/telegram-bot/internal/config/botConfig.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"pkg/configs"
+
+	"github.com/joho/godotenv"
+)
+
+type BotConfig struct {
+	BotToken    string `yaml:"bot_token"`    // BotToken - это уникальный идентификатор бота в Telegram (Выдается @BotFather при создании бота)
+	WebhookURL  string `yaml:"webhook_url"`  // Публичный HTTPS URL, на который Telegram будет отправлять обновления
+	WebhookPort string `yaml:"webhook_port"` // Локальный порт, на котором бот слушает входящие вебхуки. Обычно 8080, 8443 или 443 (для HTTPS)
+	// Стандартные значения:
+	//   - "development" - локальная разработка (больше логов, debug режим)
+	//   - "staging" - тестовый сервер (похоже на production, но с тестовыми данными)
+	//   - "production" - продакшен (минимум логов, максимум производительности)
+	Environment string `yaml:"environment"`
+}
+
+func LoadBotConfig(envPath string) (config *BotConfig, err error) {
+	// проверяем наличие пути до файла .env
+	if envPath == "" {
+		return nil, fmt.Errorf("Error during loading config: empty env file path")
+	}
+
+	err = godotenv.Load(envPath)
+	if err != nil {
+		return nil, fmt.Errorf("Error during loading config: %s\n", err.Error())
+	}
+
+	// загружаем конфиг из .yml файла
+	botConfig, err := configs.LoadYAMLConfig[BotConfig](os.Getenv("BOT_CONFIG_ADDRESS_STRING"), UseDefaultBotConfig)
+	if err != nil {
+		return nil, fmt.Errorf("Error during loading config: %s\n", err.Error())
+	}
+
+	return botConfig, nil
+}
+
+func UseDefaultBotConfig() *BotConfig {
+	return &BotConfig{
+		BotToken:    "123456:ABC",
+		WebhookURL:  "https://example.com/webhook",
+		WebhookPort: "8080",
+		Environment: "production",
+	}
+}
