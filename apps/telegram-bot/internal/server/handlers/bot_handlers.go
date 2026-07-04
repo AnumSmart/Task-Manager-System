1113e5abe95fe9a35d2a46304bbfbbe963121bee
Aliaksei Makarevich <electron2686@gmail.com>
2026-07-04 12:23:24 +0300

    updated serverl logics in telegram-bot

---
+++ apps/telegram-bot/internal/server/handlers/bot_handlers.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	tele "gopkg.in/telebot.v4"
+)
+
+// BotHttpHandler - обработчик для Telegram бота
+type BotHttpHandler struct {
+}
+
+// NewBotHttpHandler - конструктор
+func NewBotHttpHandler() *BotHttpHandler {
+	return &BotHttpHandler{}
+}
+
+// Обработка всех текстовых сообщений
+func (b *BotHttpHandler) HandleBotMessage(c tele.Context) error {
+	return nil
+}
+
+// Обработка callback-запросов от inline клавиатур
+func (b *BotHttpHandler) HandleBotCallback(c tele.Context) error {
+	return nil
+}
+
+// Обработка команды /start
+func (b *BotHttpHandler) HandleBotStart(c tele.Context) error {
+	return nil
+}
+
+// Обработка команды /help
+func (b *BotHttpHandler) HandleBotHelp(c tele.Context) error {
+	return nil
+}
