a0ba2b405570a0bea519c73b1078f9d13717a16d
Aliaksei Makarevich <electron2686@gmail.com>
2026-07-06 20:50:57 +0300

    updated deps in telegram-bot

--- apps/telegram-bot/internal/deps/di_methods.go
+++ apps/telegram-bot/internal/deps/di_methods.go
@@ -2,17 +2,21 @@ package deps
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"pkg/logger"
+	"slices"
 	grpcuserclient "telegram-bot/internal/grpc_clients/grpc_user_client"
+	"telegram-bot/internal/server"
+	"telegram-bot/internal/server/handlers"
 )
 
-// addCloser - добавляет функцию закрытия ресурса
+// addCloser - добавляет функцию закрытия ресурса.
 func (c *Container) addCloser(closer func() error) {
 	c.closers = append(c.closers, closer)
 }
 
-// initLogger - создаёт единый логгер
+// initLogger - создаёт единый логгер.
 func (c *Container) initLogger(ctx context.Context) error {
 	// Инициализируем глобальный синглтон для этого микросервиса
 	logger.InitLogger(c.config.LoggerConfig)
@@ -22,7 +26,7 @@ func (c *Container) initLogger(ctx context.Context) error {
 
 	// Проверяем, что логгер инициализирован
 	if c.logger == nil {
-		return fmt.Errorf("failed to initialize logger")
+		return errors.New("failed to initialize logger")
 	}
 
 	c.logger.Info("logger initialized",
@@ -34,7 +38,7 @@ func (c *Container) initLogger(ctx context.Context) error {
 	return nil
 }
 
-// метож инициализации grpc клиента для user-service
+// метод инициализации grpc клиента для user-service.
 func (c *Container) initGrpcUserClient(ctx context.Context) error {
 	c.logger.Info("initializing grpc client for user-service")
 
@@ -51,15 +55,57 @@ func (c *Container) initGrpcUserClient(ctx context.Context) error {
 	c.addCloser(func() error {
 		c.userGrpcClient.Close()
 		c.logger.Info("grpc user client resources cleaned up")
+
 		return nil
 	})
 
 	c.logger.Info("✅ GRPC User Client initialized")
-	return nil
 
+	return nil
 }
 
-// Close - закрытие всех ресурсов
+// метод инициализации http bot handler
+func (c *Container) initBotHandler(ctx context.Context) error {
+	c.logger.Info("initializing http bot handler")
+
+	botHttpHandler, err := handlers.NewBotHttpHandler()
+	if err != nil {
+		return err
+	}
+
+	// если все успешно, то переопределяем контейнер
+	c.botHandler = botHttpHandler
+
+	c.logger.Info("✅ Bot HTTP Handler initialized")
+	return nil
+}
+
+// метод для инициализации http сервера (botGateway)
+func (c *Container) initBotGateway(ctx context.Context) error {
+	c.logger.Info("initializing http botGateway (server)")
+
+	botGateway, err := server.NewBotGateway(ctx, c.config.BotServer, c.config.Bot, c.botHandler)
+	if err != nil {
+		return err
+	}
+	// если все успешно, то переопределяем контейнер
+	c.botGateway = botGateway
+
+	// Добавляем в closers
+	c.addCloser(func() error {
+		err := c.botGateway.Shutdown(ctx)
+		if err != nil {
+			return fmt.Errorf("botGateway shutdown - failed:%w", err)
+		}
+		c.logger.Info("botGateway resources cleaned up")
+		return nil
+	})
+
+	c.logger.Info("✅ HTTP BotGateway initialized")
+	return nil
+}
+
+// Close - закрытие всех ресурсов.
 func (c *Container) Close() error {
 	c.closeOnce.Do(func() {
 		c.logger.Info("🛑 Closing DI container resources...")
@@ -67,8 +113,9 @@ func (c *Container) Close() error {
 		var errs []error
 
 		// Закрываем ресурсы в обратном порядке
-		for i := len(c.closers) - 1; i >= 0; i-- {
-			if err := c.closers[i](); err != nil {
+		for _, v := range slices.Backward(c.closers) {
+			err := v()
+			if err != nil {
 				errs = append(errs, err)
 			}
 		}
@@ -81,4 +128,15 @@ func (c *Container) Close() error {
 	})
 
 	return c.closeErr
+}
+
+// HealthCheck проверяет здоровье зависимостей.
+func (c *Container) HealthCheck(ctx context.Context) error {
+	// TODO
+	return nil
+}
+
+// Геттер для получения экземпляра сервера из контейнера
+func (c *Container) GetBotGateway() *server.BotGateway {
+	return c.botGateway
 }
