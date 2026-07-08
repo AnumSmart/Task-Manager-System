dc71ab744d51119f746474905218c42b1927bde9
Aliaksei Makarevich <electron2686@gmail.com>
2026-07-08 20:41:38 +0300

    updated server in telegram-bot

--- apps/telegram-bot/internal/server/reg_bot_handlers.go
+++ apps/telegram-bot/internal/server/reg_bot_handlers.go
@@ -1,68 +1,22 @@
 package server
 
 import (
-	"context"
-	"log"
-
 	tele "gopkg.in/telebot.v4"
 )
 
-// registerHandlers - регистрация обработчиков бота.
+// registerHandlers - регистрация обработчиков
 func (m *PollingBotManager) registerHandlers() {
-	// 1. Команды
-	m.telegramBot.Handle("/start", func(c tele.Context) error {
-		// Отправляем задачу в канал вместо прямого вызова
-		select {
-		case m.workChan <- c:
-			return nil
-		case <-m.botCtx.Done():
-			return context.Canceled
-		default:
-			// Канал переполнен
-			c.Send("Извините, сервер перегружен. Попробуйте позже.")
-			return nil
-		}
-	})
+	// Единый обработчик для всех команд
+	submitTask := func(c tele.Context) error {
+		return m.workerPool.Submit(&BotTask{
+			ctx:     c,
+			handler: m.handler,
+		})
+	}
 
-	m.telegramBot.Handle("/help", func(c tele.Context) error {
-		select {
-		case m.workChan <- c:
-			return nil
-		case <-m.botCtx.Done():
-			return context.Canceled
-		default:
-			c.Send("Извините, сервер перегружен. Попробуйте позже.")
-			return nil
-		}
-	})
-
-	// 2. Callback
-	m.telegramBot.Handle(tele.OnCallback, func(c tele.Context) error {
-		select {
-		case m.workChan <- c:
-			return nil
-		case <-m.botCtx.Done():
-			return context.Canceled
-		default:
-			c.Respond(&tele.CallbackResponse{
-				Text: "Извините, сервер перегружен, попробуйте позже",
-			})
-			return nil
-		}
-	})
-
-	// 3. Текстовые сообщения
-	m.telegramBot.Handle(tele.OnText, func(c tele.Context) error {
-		select {
-		case m.workChan <- c:
-			return nil
-		case <-m.botCtx.Done():
-			return context.Canceled
-		default:
-			c.Send("Извините, сервер перегружен. Попробуйте позже.")
-			return nil
-		}
-	})
-
-	log.Println("📝 Обработчики бота зарегистрированы")
+	// Регистрируем все обработчики
+	m.telegramBot.Handle("/start", submitTask)
+	m.telegramBot.Handle("/help", submitTask)
+	m.telegramBot.Handle(tele.OnText, submitTask)
+	m.telegramBot.Handle(tele.OnCallback, submitTask)
 }
