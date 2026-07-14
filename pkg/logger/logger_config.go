f3fabef2cf336d4a053b598f517ce58d35f59edc
Aliaksei Makarevich <electron2686@gmail.com>
2026-07-14 19:56:22 +0300

    updated base logger in pkg

--- pkg/logger/logger_config.go
+++ pkg/logger/logger_config.go
@@ -1,11 +1,14 @@
 package logger
 
+import "pkg/elk"
+
 // LoggerConfig - конфигурация логгера
 type LoggerConfig struct {
-	Level     string `yaml:"level" json:"level"`           // debug, info, warn, error
-	Format    string `yaml:"format" json:"format"`         // json, text
-	AddSource bool   `yaml:"add_source" json:"add_source"` // добавлять файл:строку
-	Service   string `yaml:"service" json:"service"`       // имя микросервиса
+	Level     string      `yaml:"level" json:"level"`           // debug, info, warn, error
+	Format    string      `yaml:"format" json:"format"`         // json, text
+	AddSource bool        `yaml:"add_source" json:"add_source"` // добавлять файл:строку
+	Service   string      `yaml:"service" json:"service"`       // имя микросервиса
+	Elk       *elk.Config `yaml:"elk" json:"elk"`               // конфигурация Elasticsearch
 }
 
 // DefaultLoggerConfig - дефолтная конфигурация логгера
@@ -15,5 +18,6 @@ func DefaultLoggerConfig() *LoggerConfig {
 		Format:    "json",
 		AddSource: true,
 		Service:   "unknown",
+		Elk:       elk.DefaultConfig(),
 	}
 }
